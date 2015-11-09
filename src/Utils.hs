8d03f25465eab104b9ca826553d556b41fec1b7a
James Stevenson <jstevenson@safaribooksonline.com>
2015-11-09 11:53:43 -0500

    formatZonedTime function for converting timezones to a ISO-8601 formatted string.

--- src/Utils.hs
+++ src/Utils.hs
@@ -7,7 +7,10 @@ import Data.List (intercalate)
 import Data.List.Split
 import Data.List.Stream (filter, map)
 import Data.Maybe (fromJust, isJust)
+import Data.Time (ZonedTime)
+import Data.Time.Format (formatTime)
 import System.Directory (doesFileExist)
+import System.Locale (defaultTimeLocale)
 
 
 -- |Check if the given path points to a real file. Stop execution and displays an error message if not.
@@ -20,6 +23,12 @@ verifyFilePath pth = do
 -- | Format a number in the US English locale (commas separating thousands)
 formatInteger :: Int -> String
 formatInteger = reverse . (intercalate ",") . (chunksOf 3) . reverse . show
+
+
+-- | Format zoned time as a string in ISO-8601 format.
+formatZonedTime :: ZonedTime -> String
+formatZonedTime = formatTime defaultTimeLocale "%FT%T%QZ"
+
 
 
 {--
